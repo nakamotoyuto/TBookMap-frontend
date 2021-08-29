082a4cf4713734a6b8e9fa2a259d759c12219645
nakamoto_yuto <yutonakamoto03@gmail.com>
2021-08-29 13:45:12 +0900

    add recoil

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,15 @@
 import '../src/styles/globals.css'
 import type { AppProps } from 'next/app'
+import { RecoilRoot } from 'recoil';
 import { ChakraProvider } from '@chakra-ui/react'
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <ChakraProvider>
-      <Component {...pageProps} />
-    </ChakraProvider>
+    <RecoilRoot>
+      <ChakraProvider>
+        <Component {...pageProps} />
+      </ChakraProvider>
+    </RecoilRoot>
   )
 }
 export default MyApp
--- src/components/molecules/Login.tsx
+++ src/components/molecules/Login.tsx
@@ -1,5 +1,4 @@
-import { Button, FormControl, ModalBody, ModalCloseButton, ModalFooter, ModalHeader, Input, FormLabel, Box } from '@chakra-ui/react';
-import { css, jsx } from '@emotion/react';
+import { Button, FormControl, ModalBody, ModalCloseButton, ModalFooter, ModalHeader, Input, FormLabel, Box, FormErrorMessage } from '@chakra-ui/react';
 import React from 'react'
 import { useForm } from 'react-hook-form'
 
@@ -12,96 +11,57 @@ type Props = {
   modalClose: VoidFunction
 }
 
-const FormBox = css`
-  padding: 40px;
-  width: 100%;
-  text-align: center;
-  & h2{
-    margin-bottom: 40px;
-    font-size: 24px;
-  }
-`
-
-const InputBox = css`
-  margin-bottom: 30px;
-  width: 100%;
-  display: flex;
-  flex-direction: column;
-  gap: 10px;
-  text-align: left;
-  font-size: 18px;
-  font-weight: bold;
-`
-
-// const Input = css`
-//   margin-top: 10px;
-//   padding: 10px 14px;
-//   width: 100%;
-//   height: 35px;
-//   border: 1px solid #B2B2B2;
-//   border-radius: 10px;
-//   outline: none;
-//   &:focus{
-//     border: 1px solid #EB7F31;
-//   }
-// `
-
-
-
-
 
 export const Login = (props: Props) => {
   const { register, handleSubmit, formState: { errors, isValid }, reset } = useForm<loginFormInput>();
   const onSubmit = (data: loginFormInput) => {
-    reset()
-    props.modalClose()
+    console.log('test')
+    console.log(errors)
+    // reset()
+    // props.modalClose()
   }
-
-  const spanAlert = css`
-  color: red;
-  font-size: 10px;
-`
   return (
     <Box p={4}>
       <ModalHeader pl={0} pr={0} pt={4} pb={4}>Sign in your account</ModalHeader>
       <ModalCloseButton />
       <form onSubmit={handleSubmit(onSubmit)}>
-        <FormControl>
           <ModalBody>
-            <Box w={"100%"} mb={`30px`} d="flex" flexDirection="column" gap="10px" textAlign="left" fontSize="18px" fontBold="bold">
+          <FormControl isInvalid={errors.email ? true : false}>
+            <Box w={"100%"} mb={`30px`} d="flex" flexDirection="column" gap="10px" textAlign="left" fontSize="18px" fontWeight="bold">
               <FormLabel htmlFor="email">emailaddress</FormLabel>
               <Input
                 id="email"
                 type="email"
                 autoComplete="email"
-                aria-invalid={errors.email ? "true" : "false"}
                 placeholder="メールアドレスを入力してください"
-                {...register("email", { required: true, pattern: /^([a-zA-Z0-9])+([a-zA-Z0-9\._+-])*@([a-zA-Z0-9_-])+([a-zA-Z0-9\._-]+)+$/ })}
-              // css={Input}
+                {...register("email", { required:"メールアドレスは必須です。", pattern: { 
+                  value: /^([a-zA-Z0-9])+([a-zA-Z0-9\._+-])*@([a-zA-Z0-9_-])+([a-zA-Z0-9\._-]+)+$/,
+                  message: "メールアドレスの形式が間違っています"
+                }})}
               />
-              {errors.email && errors.email.type === "required" && <span css={spanAlert} role="alert">必須項目になります</span>}
-              {errors.email && errors.email.type === "pattern" && <span css={spanAlert} role="alert">メールアドレスの形式が間違っています</span>}
+              {errors.email && errors.email.type === "required" && <FormErrorMessage role="alert">必須項目になります</FormErrorMessage>}
+              {errors.email && errors.email.type === "pattern" &&  <FormErrorMessage role="alert">メールアドレスの形式が間違っています</FormErrorMessage>}
             </Box>
-            <Box w={"100%"} mb={`30px`} d="flex" flexDirection="column" gap="10px" textAlign="left" fontSize="18px" fontBold="bold">
+            </FormControl>
+            <FormControl isInvalid={errors.password ? true : false}>
+            <Box w={"100%"} mb={`30px`} d="flex" flexDirection="column" gap="10px" textAlign="left" fontSize="18px" fontWeight="bold">
               <FormLabel htmlFor="password">password</FormLabel>
               <Input
                 id="password"
                 type="password"
                 autoComplete="password"
-                aria-invalid={errors.password ? "true" : "false"}
                 placeholder="パスワードを入力してください"
-                {...register("password", { required: true, pattern: /^[a-z\d]{1,100}$/i })}
-              // css={Input}
+                {...register("password", { required: true, pattern: /^[a-z\d]{2,100}$/i })}
               />
-              {errors.password && errors.password.type === "required" && <span css={spanAlert} role="alert">必須項目になります</span>}
-              {errors.password && errors.password.type === "pattern" && <span css={spanAlert} role="alert">半角英数字で入力お願いします</span>}
+              {errors.password && errors.password.type === "required" && <FormErrorMessage role="alert">必須項目になります</FormErrorMessage>}
+              {errors.password && errors.password.type === "pattern" && <FormErrorMessage role="alert">半角英数字で入力お願いします</FormErrorMessage>}
             </Box>
+            </FormControl>
           </ModalBody>
           <ModalFooter>
-            <Button maxWidth={250} mr={3} backgroundColor={`#EB7F31`} color={`#ffffff`}>ログイン</Button>
+            <Button type="submit" maxWidth={250} mr={3} backgroundColor={`#EB7F31`} color={`#ffffff`}>ログイン</Button>
             <Button onClick={props.modalClose}>Close</Button>
           </ModalFooter>
-        </FormControl>
       </form>
     </Box>
   )
