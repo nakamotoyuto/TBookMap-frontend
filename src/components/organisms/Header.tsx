39a6e09b34c9fcfe0225e6d1365836e4081f6d61
nakamoto_yuto <yutonakamoto03@gmail.com>
2021-08-29 13:48:29 +0900

    fix header

--- src/components/organisms/Header.tsx
+++ src/components/organisms/Header.tsx
@@ -5,22 +5,17 @@ import { BookRequestButton } from '../molecules/BookRequestButton'
 import { LoginButton } from '../molecules/LoginButton'
 
 export const Header = () => {
-  const HeaderRightBox = css`
-    display:flex;
-    align-items:center;
-    gap: 0 10px;
-  `
   return (
     <Box p={`20px`} w={`100%`} d="flex" justifyContent="space-between" alignItems="center" borderBottom={`1px`} borderColor={`#B2B2B2`}>
       <Heading as="h1">
         sansaku
       </Heading>
-      <div css={HeaderRightBox}>
+      <Box d={"flex"} alignItems={"center"} gap={"0 10px"}>
         <img src={`/img/svg/search.svg`} />
         <img src={`/img/svg/myicon.svg`} />
         <LoginButton />
         <BookRequestButton />
-      </div>
+      </Box>
     </Box>
   )
 }
