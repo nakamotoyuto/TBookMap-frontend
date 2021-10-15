e2c91a6eee6f36675836ce43312ab0e18f0b18c5
nakamoto_yuto <yutonakamoto03@gmail.com>
2021-10-15 22:28:43 +0900

    fix constants

--- src/util/constants.ts
+++ src/util/constants.ts
@@ -1,5 +1,3 @@
-export const API_URL = process.env.NODE_ENV === "production" ?
-  "http://localhost:8000/api/" : process.env.API_URL
-export const IMAGE_URL = process.env.NODE_ENV === "production" ?
-  "http://localhost:8000/images/" : process.env.IMAGE_URL
-  
+export const API_URL = process.env.API_URL
+
+export const IMAGE_URL = process.env.IMAGE_URL
