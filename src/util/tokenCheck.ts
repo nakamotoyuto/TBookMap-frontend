a45dbd0e51f0e9d62e413ef042613db1ecdd10eb
nakamoto_yuto <yutonakamoto03@gmail.com>
2021-11-20 19:04:54 +0900

    add token check function

--- src/util/tokenCheck.ts
+++ src/util/tokenCheck.ts
@@ -2,13 +2,16 @@ import { useRouter } from "next/router";
 import Cookies from 'js-cookie'
 
 type TokenCheckType = {
-  (path: string): void
+  (path: string): void | string
 }
 
 export const tokenCheck: TokenCheckType = (path) => {
   const router = useRouter();
   //Cookieのチェック（これをいろいろ認証タイプにより変更）
   const token = Cookies.get("sansakuToken");
-  console.log(token)
-  if (token === undefined) router.replace(path);
+  if (token === undefined) {
+    router.replace(path);
+    return
+  }
+  return token
 }
