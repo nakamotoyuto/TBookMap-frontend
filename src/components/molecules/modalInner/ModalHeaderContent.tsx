96ca17a2364cfbb319eed71a6f0b59f5df11757b
nakamoto_yuto <yutonakamoto03@gmail.com>
2022-03-24 00:20:00 +0900

    fix: modalHeader chakra削除

--- src/components/molecules/modalInner/ModalHeaderContent.tsx
+++ src/components/molecules/modalInner/ModalHeaderContent.tsx
@@ -1,17 +1,17 @@
-import { ModalHeader, ModalCloseButton } from '@chakra-ui/modal';
-import React from 'react';
+import React, { MouseEventHandler } from 'react';
 
 type Props = {
   modalHeader: string
+  onClick?: MouseEventHandler<HTMLButtonElement>;
 }
 
-export const ModalHeaderContent = ({modalHeader}: Props) => {
+export const ModalHeaderContent = ({ modalHeader, onClick }: Props) => {
   return (
     <>
-      <ModalHeader pl={0} pr={0} pt={4} pb={4} d="flex" justifyContent="space-between">
+      <div className='pl-0 pr-0 pt-4 pb-4 flex justify-between'>
         {modalHeader}
-        <ModalCloseButton />
-      </ModalHeader>
+        <button onClick={onClick}>x</button>
+      </div>
     </>
   );
 };
